53654b9d6d995de2a55c9962b3014242646080f2
mizukiokumura <nihonnpart4@yahoo.co.jp>
2021-02-23 15:33:50 +0900

    memory投稿機能とmemory編集、削除、詳細ページの実装

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,4 +1,51 @@
 class MemoriesController < ApplicationController
+  before_action :authenticate_user!,   only: [:edit, :new, :destroy, :update]
+  before_action :set_memory,           only: [:show, :edit, :update, :destroy]
   def index
+    @memories = Memory.includes(:user).order(id: "DESC").limit(4)
+  end
+
+  def new
+    @memory = Memory.new
+  end
+
+  def show
+  end
+
+  def create
+    @memory = Memory.new(memory_params)
+    if @memory.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @memory.update(memory_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @memory.destroy
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def memory_params
+    params.require(:memory).permit(:title, :memory, :image, :diary).merge(user_id: current_user.id)
+  end
+
+  def set_memory
+    @memory = Memory.find(params[:id])
   end
 end
---
+++ db/migrate/20210222021438_create_memories.rb
@@ -0,0 +1,11 @@
+class CreateMemories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :memories do |t|
+      t.string :title,    null: false
+      t.text :diary,      null: false
+      t.date   :memory,   null: false
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
